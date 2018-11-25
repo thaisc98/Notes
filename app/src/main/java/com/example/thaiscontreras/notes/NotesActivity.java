80f50bd5dbd19425ffc8d60c2088f047b8d8badf
Thais Contreras <thaisc98@hotmail.es>
2018-11-24 18:56:20 -0600

    Añado lista nota con una clase Nota

--- app/src/main/java/com/example/thaiscontreras/notes/NotesActivity.java
+++ app/src/main/java/com/example/thaiscontreras/notes/NotesActivity.java
@@ -2,13 +2,32 @@ package com.example.thaiscontreras.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NotesActivity extends AppCompatActivity {
+
+    private ArrayList<Nota> notas;
+    private ListView listNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        notas = new ArrayList<>();
+        notas.add(new Nota("Hola","klk"));
+        notas.add(new Nota("jeje","sdadsa"));
+
+        listNotes = (ListView)findViewById(R.id.ListNotes);
+        listNotes.setAdapter(new ArrayAdapter<>(
+                    this,android.R.layout.simple_list_item_1, notas
+        ));
+
+
     }
 
 }
